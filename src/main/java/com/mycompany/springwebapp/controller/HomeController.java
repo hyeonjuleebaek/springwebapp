8ceabf1967ef45f4923bded54d86f2fa9889176f
hyeonjuleebaek <hyeonjuleebaek@gmail.com>
2023-07-19 10:26:00 +0900

    component-scan 적용

--- src/main/java/com/mycompany/springwebapp/controller/HomeController.java
+++ src/main/java/com/mycompany/springwebapp/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.mycompany.springwebapp.controller;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,22 @@ public class HomeController {
 	public HomeController() {
 		log.info("실행");
 	}
+	
+	@PostConstruct
+	public void method1() {
+		log.info("실행");
+	}
+	
+	@PostConstruct
+	public void method2() {
+		log.info("실행");
+	}
+	
+	@PreDestroy
+	public void method3() {
+		
+	}
+	
 	//http://localhost:8080/springwebapp/
 	@RequestMapping("/")
 	public String index() {
