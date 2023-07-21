7df52ef7377c4afc2cdadf0d52b1fcb8509f8198
hyeonjuleebaek <hyeonjuleebaek@gmail.com>
2023-07-21 12:20:17 +0900

    Ch04Controller 수정

--- src/main/java/com/mycompany/springwebapp/controller/Ch04Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch04Controller.java
@@ -1,10 +1,16 @@
 package com.mycompany.springwebapp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.mycompany.springwebapp.dto.Ch04Dto;
+import com.mycompany.springwebapp.dto.Ch04Form1;
+import com.mycompany.springwebapp.validator.Ch04Form1Validator;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,14 +23,28 @@ public class Ch04Controller {
 		return "ch04/content";
 	}
 	
+	@InitBinder("ch04Form1")
+	public void Ch04Form1Validator(WebDataBinder binder) {
+		binder.setValidator(new Ch04Form1Validator());
+	}
+	
 	@PostMapping("/method1")
-	public String method1(Ch04Dto dto) {
-		log.info("parama1: " + dto.getParam1());
-		log.info("parama2: " + dto.getParam2());
-		log.info("parama3: " + dto.getParam3());
-		log.info("parama4: " + dto.isParam4());
-		log.info("parama5: " + dto.getParam5());
-		return "redirect:/ch04/content";
+	//pom.xml에 validation-api 라이브러리 의존 설정 필요
+	//request.setAttribute("ch04Form1", form1);로 자동저장
+	public String method1(@Valid Ch04Form1 form1, Errors errors) {
+		//errors.rejectValue가 한번이라도 호출되었다면 hasErrors는 true를 리턴
+		if(errors.hasErrors()) {
+			//폼으로 다시 돌려보냄
+			return "ch04/content";
+		}
+		
+		//요청 처리 코드
+		log.info("param1: " + form1.getParam1());
+		log.info("param2: " + form1.getParam2());
+		log.info("param3: " + form1.getParam3());
+		log.info("param4: " + form1.isParam4());
+		log.info("param5: " + form1.getParam5());
+		return "redirect:/";
 	}
 	
 }
