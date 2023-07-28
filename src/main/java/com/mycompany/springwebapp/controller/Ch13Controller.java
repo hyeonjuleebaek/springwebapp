7781dabdafa0b4256456b614f3568497ae65e119
hyeonjuleebaek <hyeonjuleebaek@gmail.com>
2023-07-28 17:23:52 +0900

    Ch13Controller 생성 (@Mapper, @Service)

--- src/main/java/com/mycompany/springwebapp/controller/Ch13Controller.java
+++ src/main/java/com/mycompany/springwebapp/controller/Ch13Controller.java
@@ -1,14 +1,20 @@
 package com.mycompany.springwebapp.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.mycompany.springwebapp.dao.Ch13BoardDaoOld;
+import com.mycompany.springwebapp.dao.Ch13BoardDao;
+import com.mycompany.springwebapp.dao.Ch13BoardDaoOldImpl;
 import com.mycompany.springwebapp.dto.Ch13Board;
+import com.mycompany.springwebapp.dto.Ch13Pager;
+import com.mycompany.springwebapp.service.Ch13BoardService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,42 +23,61 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/ch13")
 public class Ch13Controller {
 	@Resource
-	private Ch13BoardDaoOld boardDaoOld;
+	private Ch13BoardService boardService;
+
+	@Autowired
+	private Ch13BoardDao boardDao;
 	
-   @RequestMapping("/content")
-   public String content(HttpSession session) {
-      return "/ch13/content";
-   }
-   
-   @GetMapping("/insertBoard")
-   public String insertBoard() {
-	   log.info("실행");
-	   
-	   Ch13Board board = new Ch13Board();
-	   board.setBtitle("괴롭지?");
-	   board.setBcontent("한번만 성공해, 그뒤로는 쉬워");
-	   board.setMid("user");
-	   
-	   boardDaoOld.insert(board);
-	   
-	   return "redirect:/ch13/content";
-   }
-   
-   @GetMapping("/getBoardList")
-   public String getBoardList() {
-	   boardDaoOld.selectAll();
-	   return "redirect:/ch13/content";
-   }
-   
-   @GetMapping("/updateBoard")
-   public String updateBoard() {
-	   boardDaoOld.updateByBno();
-	   return "redirect:/ch13/content";
-   }
-   
-   @GetMapping("/deleteBoard")
-   public String deleteBoard() {
-	   boardDaoOld.deleteByBno();
-	   return "redirect:/ch13/content";
-   }
+	@RequestMapping("/content")
+	public String content(HttpSession session) {
+		return "/ch13/content";
+	}
+
+	@GetMapping("/insertBoard")
+	public String insertBoard() {
+		log.info("실행");
+		
+			Ch13Board board = new Ch13Board();
+			board.setBtitle("행복해");
+			board.setBcontent("고생한 만큼, 실력을 향상시켜서  연봉" + 1000000000 + "을 받을거야.");
+			board.setMid("user");
+			
+			boardService.write(board);
+			
+			//실제로 저장된 bno
+			log.info("저장된 bno: " + board.getBno());
+		
+		return "redirect:/ch13/content";
+	}
+
+	@GetMapping("/getBoardList")
+	public String getBoardList() {
+		int totalRows = boardService.getTotalBoardNum();
+		Ch13Pager pager = new Ch13Pager(10, 5, totalRows, 1);
+		
+		List<Ch13Board> list = boardService.getList(pager);
+		
+		for(Ch13Board board : list) {
+			log.info(board.toString());
+		}
+		
+		return "redirect:/ch13/content";
+	}
+
+	@GetMapping("/updateBoard")
+	public String updateBoard() {
+		Ch13Board board = boardService.getBoard(10001);
+		board.setBtitle("오늘은 금요일");
+		board.setBcontent("아침부터 안되네, 열심히 고쳐보자");
+
+		boardService.modify(board);
+		return "redirect:/ch13/content";
+	}
+
+	@GetMapping("/deleteBoard")
+	public String deleteBoard() {
+		int bno = 10001;
+		boardService.remove(bno);
+		return "redirect:/ch13/content";
+	}
 }
---
+++ src/main/java/com/mycompany/springwebapp/dto/Ch13Member.java
@@ -0,0 +1,13 @@
+package com.mycompany.springwebapp.dto;
+
+import lombok.Data;
+
+@Data
+public class Ch13Member {
+	public String mid;
+	public String mname;
+	public String mpassword;
+	public boolean menabled;
+	public String mrole;
+	public String memail;
+}
